132acbced219c6594151e601e7711a2f038051f1
brianaabreu <briana.abreu01@gmail.com>
2021-08-12 14:34:30 -0700

    finished

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+const app = express();
 
 import postRoutes from './routes/posts.js';
 
-const app = express();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
